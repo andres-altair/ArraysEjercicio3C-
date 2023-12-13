f7492271b51bc8f21d437efd946c8955af574ae3
DESKTOP-JOOKDG6\profesor <csi23-amoliaz@colegioaltair.net>
2023-12-13 10:15:12 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+
+namespace ArraysEjercicio3C
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int[,] ar = new int[10,10];
+            for (int i = 100; i < ar.Length; i--)
+            {
+                ar[i,-i] = i;
+                Console.WriteLine(ar[i].ToString());
+            }
+           for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = 0; j < ar[i].Length; j++)
+                {
+                    Console.WriteLine(i,j);
+                }
+            }
+        }
+
+    }
+}
